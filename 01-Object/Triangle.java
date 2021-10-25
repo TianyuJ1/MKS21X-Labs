dd08a1f6a65b57c11e958fd22e69d9ab6ac15c30
Tianyu Jiao <tjiao30@stuy.edu>
2021-10-25 16:32:02 -0400

    finished second constructor

--- 01-Object/Triangle.java
+++ 01-Object/Triangle.java
@@ -6,4 +6,12 @@ public class Triangle{
 		v2 = p2;
 		v3 = p3;
 	}
+	
+	public Triangle(double x1, double y1, double x2, double y2, double x3, double y3){
+		v1 = new Point (x1, y1);
+		v2 = new Point (x2, y2);
+		v3 = new Point (x3, y3);
+	}
 }
+
+
