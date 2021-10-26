f288a2cbd4b3e75cc90c3cb2f0c5865924906c55
Tianyu Jiao <tjiao30@stuy.edu>
2021-10-26 17:01:22 -0400

    equal boolean finished

--- 02-Triangle/Point.java
+++ 02-Triangle/Point.java
@@ -31,4 +31,47 @@ public class Point{
    public String toString(){
    	return "(" + x + ", " + y + ")";
    }
+   	public static boolean closeEnough(double a, double b){
+  //How can you determine if two values are close enough to eachother?
+  		if ((a / b * 100 <= 100.001) && (a / b * 100 >= 99.999)){
+  		return true;
+  		}else{
+  		return false;
+  		}
+  	}
+  	/*Return true when the respective x and y values of the two points differ by at most 0.001% each.
+*To avoid issues: If one is 0.0, the other must be exactly 0.0.
+*/
+	public boolean equals(Point other){
+		boolean xSame;
+		boolean ySame;
+		
+		if (this.getX() == 0 || other.getX() == 0){
+		
+		if (this.getX() == 0 && other.getX() == 0){
+		xSame = true;
+		}else{
+		xSame = false;
+		}}else{
+		xSame = closeEnough(this.getX(), other.getX());
+		}
+		
+	
+		
+		if (this.getY() == 0 || other.getY() == 0){
+		
+		if (this.getY() == 0 && other.getY() == 0){
+		ySame = true;
+		}else{
+		ySame = false;
+		}}else{
+		ySame = closeEnough(this.getY(), other.getY());
+		}
+		
+		
+		return (xSame && ySame);
+		}
+		
+
 }
+
