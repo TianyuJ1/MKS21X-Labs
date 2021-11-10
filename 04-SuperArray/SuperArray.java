f96df300225448c959c33811d0745daf501c9283
Tianyu Jiao <tjiao30@stuy.edu>
2021-11-10 10:14:55 -0500

    fixed toStringDebug

--- 04-SuperArray/SuperArray.java
+++ 04-SuperArray/SuperArray.java
@@ -1,48 +1,57 @@
-public class SuperArray  { 
+public class SuperArray  {
 
   private String[] data;
   private int size;
-  
+
     public SuperArray() {
   	data = new String[10];
   	size = 0;
   }
-  public int size() {
+    public int size() {
     return size;
   }
 
-  public boolean add(String s){
-  
+    public boolean add(String s){
+
     for (int x = 0; x <= data.length-1; x++){
       if (data[x] == null) {
-       data[x] = s;
-        size ++;
-      }       
+      data[x] = s;
+      size ++;
+      }
    }
-   return true;
+    return true;
   }
 
- public String toString() {
-   String toString = "[";
-   for (int x = 0; x < size; x++){
-     if (data[x] != null){
-     toString += data[x];
-       toString += ",";
-     }else if (data[x] == null){
-     toString += "";
-   }
-   toString += "]";
-   return toString;
-  }
- 
- return toString;
- }
+    public String toString() {
+      String toString = "[";
+      for (int x = 0; x < size; x++){
+        if (data[x] != null && data[x] +1 != null){
+          toString += data[x];
+          toString += ",";
+        }else if (data[x] == null){
+      toString += "";
+      }
+      toString += "]";
+      return toString;
+    }
+
+    return toString;
+    }
+
+    public String toStringDebug() {
+      String toStringDebug = "[";
+      for (int i = 0; i < data.length; i++){
+            toStringDebug += data[i];
+            toStringDebug += ", ";
+      }
+      toStringDebug += "]";
+      return toStringDebug;
+    }
+
+
+
+
 /*
- public String toStringDebug() {
-   String Debug = "[";
-   for (int x = 0; x < data.length - 1; x++){
-     Debrivate String[] data
-
  private int Debug += data[x];
      Debug += ",";
    }
@@ -85,7 +94,8 @@ public class SuperArray  {
 
     public String remove(int index) {
       String removeData = data[index];
-      data[index] = null;
+      dat          if (data[x] == null || data[x] != null){
+a[index] = null;
       for (int x = data[index]; x < data.length -1; x++){
         data.[x] = removeData[x]+1;
       return removeData;
@@ -111,4 +121,3 @@ public class SuperArray  {
 
 
 }
-
