9dfa35a7c29a1faded5d01a8c05f35d70dd8322a
TianyuJ1 <90489328+TianyuJ1@users.noreply.github.com>
2021-11-17 08:36:50 -0500

    Fixed resize method

--- 04-SuperArray/SuperArray.java
+++ 04-SuperArray/SuperArray.java
@@ -83,13 +83,36 @@ public class SuperArray  {
       data=new String[initialCapacity];
     }
 
-    private void resize() {
-      String[] resizeData = new String[2 *data.length +1];
-      for (int x = 0; x < data.length; x++){
-        resizeData[x] = data[x];
+        private void resize() {
+        String [] resizing = java.util.Arrays.copyOf(data,data.length);
+        data = new String[data.length+1];
+        data = java.util.Arrays.copyOf(resizing,data.length);
+    }
+    public String remove(int index) {
+        /*if (index > data.length || index < 0) {
+            System.out.println("error");
+        }*/
+
+        String dataindex = data[index];
+        if (index == size()-1) {
+            String[] copy = Arrays.copyOfRange(data, 0, size-2);
+            data = java.util.Arrays.copyOfRange(copy,0,size);
+            size--;
+            return dataindex;
         }
-     data = resizeData;
-   }
+        String[] copy = Arrays.copyOfRange(data, index+1, data.length-1);
+        data = java.util.Arrays.copyOfRange(copy,0,data.length-1);
+        size--;
+        return dataindex;
+    }
+    public int indexOf(String s) {
+        for (int x = 0; x < data.length; x ++) {
+            if (data[x].equals(s)) {
+                return x;
+            }
+        }
+        return -1;
+    }
 /*
     public String remove(int index) {
       String removeData = data[index];
