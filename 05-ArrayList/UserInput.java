11fab852654b0cd3991e7bebd9f78481d40148ea
TianyuJ1 <90489328+TianyuJ1@users.noreply.github.com>
2021-11-22 05:54:15 -0500

    working on step 2

---
+++ 05-ArrayList/UserInput.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+  public class UserInput {
+    public static void main(String[] args){
+      
+      for int (i = 0; i < size(); i ++){
+        if (i > 0) {
+          System.out.println("Enter a non negative number. A negative number will end the program:" + i);
+        }else{
+          
