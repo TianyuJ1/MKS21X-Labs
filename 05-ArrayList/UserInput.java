5aaa7b71fa73034465efadbad7d230acd6648b40
TianyuJ1 <90489328+TianyuJ1@users.noreply.github.com>
2021-11-22 06:11:56 -0500

    working on mean method

--- 05-ArrayList/UserInput.java
+++ 05-ArrayList/UserInput.java
@@ -1,9 +1,14 @@
 import java.util.ArrayList;
   public class UserInput {
     public static void main(String[] args){
-      
+      double[] doubleArray = [10.5, 9.5, 9.0, 10.0, 11.0];
+      double sum = 0;
       for int (i = 0; i < size(); i ++){
         if (i > 0) {
+          data[x] = i;
+          sum += i;
           System.out.println("Enter a non negative number. A negative number will end the program:" + i);
-        }else{
+        }
+        double mean = sum / (size() -1);
+        
           
