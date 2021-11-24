6b1140907c72a43a923c26278cf6460455417b81
TianyuJ1 <90489328+TianyuJ1@users.noreply.github.com>
2021-11-24 10:07:07 -0500

    working on pt2

--- Triangles.java
+++ Triangles.java
@@ -36,7 +36,49 @@
       return ValidTs;
     }
     
-    
+    public int ValidColumns(File data){
+        int ValidCs = 0;
+        try{
+          File file = new File("data");
+          Scanner input = new Scanner(file);
+          if(input.hasNextInt() == true){
+            int c1 = input.nextInt();
+            int c2 = input.nextInt();
+            int c3 = input.nextInt();
+            int c4 = input.nextInt();
+            int c5 = input.nextInt();
+            int c6 = input.nextInt();
+            int c7 = input.nextInt();
+            int c8 = input.nextInt();
+            int c9 = input.nextInt();
+
+          if ((c1 + c2) > c3 || (c2 + c3) > c1 || (c1 + c3) > c2)
+          && if ((c4 + c5) > c6 || (c4 + c6) > c5 || (c5 + c6) > c4)
+          && if ((c7 + c8) > c9 || (c8 + c9) > c7 || (c7 + c9) > c8){
+            Valids += 1;
+          }else{
+            System.out.println("invalid");
+          }
+        }
+      }
+      Input.close();
+      catch{(FileNotFoundException ex)
+        System.out.println("file not found");
+    }
+    return ValidCs;
+
+        /*
+        for (int = 0; i < data.size(); i ++){
+          if (a + b > z || a + c > b || b + c > a){
+            ValidTs += 1;
+          }else{
+            System.out.println("invalid");
+          }
+        }*/
+      }
+
+
+      }
         
 }
        
