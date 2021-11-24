641eedbe074b116f532624366cb90bc75f680d8f
TianyuJ1 <90489328+TianyuJ1@users.noreply.github.com>
2021-11-24 06:24:40 -0500

    working on part 1

---
+++ Triangles.java
@@ -0,0 +1,27 @@
+ import java.io.File;
+  import java.io.FileNotFoundException;
+  import java.util.Scanner;
+  public class ReadFile {
+    public static void main(String[] args) {
+        try {
+            File file = new File("data");
+            Scanner input = new Scanner(file);
+            while (input.hasNextLine()) {
+                String line = input.nextLine();
+                System.out.println(line);
+            }
+            input.close();//releases the file from your program
+
+        } catch (FileNotFoundException ex) {
+            System.out.println("file not found");
+        }
+    }
+    public static int ValidTrianles(File data){
+      int ValidTs = 0;
+      try{
+        File file = new File("data.txt");
+        Scanner input = new Scanner(file);
+        int x = input.nextInt();
+        int y = input.nextInt();
+        int z = input.nextInt();
+        
