42a0f2bf56833dc50170979016b389aa2045cd1b
Tianyu Jiao <tjiao30@stuy.edu>
2021-12-01 09:54:59 -0500

    created a test case

---
+++ TesterSorts.java
@@ -0,0 +1,8 @@
+public class TesterSorts{
+  public static void main(String[] args) {
+
+    ArrayList example = new ArrayList(0, 0, 0, 10, 0, );
+
+    System.out.println(ArrayList.bubbleSort());replaceEmpty
+  }
+}
