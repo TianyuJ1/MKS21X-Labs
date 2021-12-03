e1ea268ae0d8d4909c8db8be654a15f98d9008e9
Tianyu Jiao <tjiao30@stuy.edu>
2021-12-03 10:15:36 -0500

    woring on selectionSort mathod

---
+++ 08-Sorts/selectionSort.java
@@ -0,0 +1,18 @@
+import java.util.Collections;
+import java.util.Arrays;
+public class selectionSort
+{
+  public static void main(String[] args){
+    Integer[] num={1, 2, 4, 3};
+    }
+
+  public static void selectionSort(int [] ary) {
+    for (int i = 0; i < arr.length; i++){
+      data[i] = Collections.min(ary(num));
+    }
+    for (int j = 1; j < arr.length; j++){
+      data[i +1] = Collections.min(ary(num));
+    }
+
+  }
+  }    //Yes there is camel case.
