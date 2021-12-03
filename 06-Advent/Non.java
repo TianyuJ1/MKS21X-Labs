5e467b96af2baf5beb87b68d34a5b5db0b3ea0c1
TianyuJ1 <90489328+TianyuJ1@users.noreply.github.com>
2021-12-03 09:56:32 -0500

    Update Non.java

--- 06-Advent/Non.java
+++ 06-Advent/Non.java
@@ -1,50 +1,111 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-public class Non {
-    public static void main(String[] args){
-      try {
-          File file = new File("data set2");
-          Scanner input = new Scanner(file);
-          int lineNum = 0;
-      }
-                  System.out.println("nice string " + NiceString(file));
+import java.util.*;
+public class Non
+{
+    public static void main(String[] args)throws FileNotFoundException{
+    	try{
+    		File file = new File("dataSet2");
+    		System.out.println(NiceString(file));
+    	}catch(FileNotFoundException ex){
+    		System.out.println("File not found");
+    	}
+    }
+
+   public static int NiceString(File data)throws FileNotFoundException{
+
+        Scanner input = new Scanner(data);
+        int counter = 0;
+        while (input.hasNext()){
+     	    int vowels = 0;
+     	    int repeats = 0;
+     	    int found = 0;
+        //try every pair of letter   
+        String n = input.next();
+        for (int j = 0; j < n.length(); j ++){
+        	if (n.charimport java.util.ArrayList;
+import java.util.Collections;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.*;
+public class Non
+{
+    public static void main(String[] args)throws FileNotFoundException{
+    	try{
+    		File file = new File("dataSet2");
+    		System.out.println(NiceString(file));
+    	}catch(FileNotFoundException ex){
+    		System.out.println("File not found");
+    	}
+    }
+
+   public static int NiceString(File data)throws FileNotFoundException{
+
+        Scanner input = new Scanner(data);
+        int counter = 0;
+        while (input.hasNext()){
+     	    int vowels = 0;
+     	    int repeats = 0;
+     	    int found = 0;
+        //try every pair of letter   
+        String n = input.next();
+        for (int j = 0; j < n.length(); j ++){
+        	if (n.charAt(j) == 'a'||n.charAt(j) == 'e' ||n.charAt(j) == 'i'||n.charAt(j) == 'o'||n.charAt(j) == 'u'){
+         		vowels += 1;
+		}
+	for (int k = 0; k < n.length()-1; k ++){
+		if (n.charAt(k) == n.charAt(k+1)){
+		repeats = 1;
+		}
+	}
+	for (int h = 0; h < n.length()-1; h++){
+		if(n.substring(h, h + 2).equals("ab") || n.substring(h, h + 2).equals("cd") || n.substring(h, h + 2).equals("pq") || n.substring(h, h + 2).equals("xy")){
+		found = 1;
+		}
+	}
+	}
+	if((vowels >= 3) && (repeats == 1) && (found == 0)){
+  		counter++;
+
+  	
+  
 
 
-          while (input.hasNextLine()) {
-              String line = input.nextLine();
-              System.out.println(lineNum + ":  " + line);
-              lineNum++;
-          }
-        
-          
-          input.close();
+  }
+  }
+  input.close();
+ return counter;
+ 
+ }
+ }At(j) == 'a'||n.charAt(j) == 'e' ||n.charAt(j) == 'i'||n.charAt(j) == 'o'||n.charAt(j) == 'u'){
+         		vowels += 1;
+		}
+	for (int k = 0; k < n.length()-1; k ++){
+		if (n.charAt(k) == n.charAt(k+1)){
+		repeats = 1;
+		}
+	}
+	for (int h = 0; h < n.length()-1; h++){
+		if(n.substring(h, h + 2).equals("ab") || n.substring(h, h + 2).equals("cd") || n.substring(h, h + 2).equals("pq") || n.substring(h, h + 2).equals("xy")){
+		found = 1;
+		}
+	}
+	}
+	if((vowels >= 3) && (repeats == 1) && (found == 0)){
+  		counter++;
 
-       } catch (FileNotFoundException ex){ 
-          System.out.println("file not found");
-      	}
-      }
+  	
+  
 
-      public static String NiceString(File data){
-        String vowelLetter = new[] { 'a', 'e', 'i', 'o', 'u' };
-        String naughtyLetter = new[] {'ab', 'cd','pq','xy'}
-        int vowel = 0;
-        int repeat = 0;
-        int naughty = 0;
-        while(input.hasNextLine()){
-          String k = input.hasNextline();
-          if (k == vowelLetter){
-            vowel += 1;  
-          }
-          if (k == input.charAt(k + 1)){
-            repeat += 1;
-          }
-          if (k == naughtyLetter){
-            naughty += 1;
-          }
-          if (vowel >= 3 && repeat >= 2 && naughty == 0){
-           return input.nextLine();
-          }
-        }
-      }
-        
+
+  }
+  }
+  input.close();
+ return counter;
+ 
+ }
+ }     
