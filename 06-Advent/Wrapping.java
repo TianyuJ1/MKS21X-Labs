d5477827c72260cdd609cf5cfe9903541e31dcea
TianyuJ1 <90489328+TianyuJ1@users.noreply.github.com>
2021-12-07 10:02:19 -0500

    Update Wrapping.java

--- 06-Advent/Wrapping.java
+++ 06-Advent/Wrapping.java
@@ -2,9 +2,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class Wrapping {
-	public static void main(String[] args){
-	 try{
-	  File file = new File("file2");
+public static void main(String[] args){
+/*try{*/
+ /*File file = new File(args[0]);
           Scanner input = new Scanner(file);
           int lineNum = 0;
 
@@ -12,42 +12,52 @@ public class Wrapping {
               String line = input.nextLine();
               System.out.println(lineNum + ":  " + line);
               lineNum++;
-          }
-          System.out.println(slacks(file));
-	 }
-	 catch(FileNotFoundException e){
-        		System.out.println("file not found");
-    		}
-	}
-		
-		public static int slacks(File data){
-			int slack = 0;
-			
-			try {
-				
-          			File file = new File("file2");
-          			Scanner input = new Scanner(file);
-          			while (input.hasNextInt()){
-          				int max = 0;
-          				int length = input.nextInt();
-          				int width = input.nextInt();
-            				int height = input.nextInt();
-            				if (length >= width && length >= height){
-            					max = length;
-            				}
-            				if (height >= width && height >= length){
-            					max = height;
-            				}
-            				if (width >= length && width >= height){
-            					max = width;
-            				}
-            				slack = slack + ((length + width + height - max)*2 + length * width * height);
-            			
-            	}
-            	}
-            	catch(FileNotFoundException e){
-        		System.out.println("file not found");
-    		}
-    	return slack;
-    	}
+          }*/
+          System.out.println(slacks(args[0]));
+/* }
+catch(FileNotFoundException e){
+        System.out.println("file not found");
+    }*/
+}
+
+public static int slacks(String data){
+int slack = 0;
+
+try {
+
+          File file = new File(data);
+          Scanner input = new Scanner(file);
+          while (input.hasNextLine()){
+          String line = input.nextLine();
+          Scanner s = new Scanner(line);
+          int firstX = line.indexOf("x");
+          String lengthString = line.substring(0, firstX);
+          int length = Integer.parseInt(lengthString);
+         
+         
+          int secondX = line.lastIndexOf("x");
+          int width = s.nextInt();
+            int height = s.nextInt();
+          int max = 0;
+       
+         
+            System.out.println(length + " " + width + " " + height);
+            if (length >= width && length >= height){
+            max = length;
+            }
+            if (height >= width && height >= length){
+            max = height;
+            }
+            if (width >= length && width >= height){
+            max = width;
+            }
+            slack = slack + ((length + width + height - max)*2 + length * width * height);
+           
+            }
+            }
+            catch(FileNotFoundException e){
+        System.out.println("file not found");
+    }
+    return slack;
+    }
     }
