fe87c94ff5a55b3db092930200222dc2d64b07cd
TianyuJ1 <90489328+TianyuJ1@users.noreply.github.com>
2021-12-07 10:50:34 -0500

    Update Wrapping.java

--- 06-Advent/Wrapping.java
+++ 06-Advent/Wrapping.java
@@ -19,7 +19,36 @@ catch(FileNotFoundException e){
         System.out.println("file not found");
     }*/
 }
+public static int wrapPaper(String data){
+int WrapPaper = 0;
 
+try {
+
+          File file = new File(data);
+          Scanner input = new Scanner(file);
+          while (input.hasNextLine()){
+          String line = input.nextLine();
+          Scanner s = new Scanner(line);
+          int firstX = line.indexOf("x");
+          int secondX = line.lastIndexOf("x");
+          
+          String lengthString = line.substring(0, firstX);
+          int length = Integer.parseInt(lengthString);
+          
+          String widthString = line.substring(firstX, secondX);
+          int width = Integer.parseInt(widthString);
+          
+          String heightString = line.substring(secondX, line.size());
+          int height = Integer.parseInt(heightString);
+          
+           WrapPaper = ((2 * length * height) + (2 * length * width) + (2 * height * width));\}
+} catch(FileNotFoundException e){
+        System.out.println("file not found");
+    }
+    return slack;
+    }
+           
+           
 public static int slacks(String data){
 int slack = 0;
 
@@ -31,17 +60,18 @@ try {
           String line = input.nextLine();
           Scanner s = new Scanner(line);
           int firstX = line.indexOf("x");
+          int secondX = line.lastIndexOf("x");
+          
           String lengthString = line.substring(0, firstX);
           int length = Integer.parseInt(lengthString);
-         
-         
-          int secondX = line.lastIndexOf("x");
-          int width = s.nextInt();
-            int height = s.nextInt();
+          
+          String widthString = line.substring(firstX, secondX);
+          int width = Integer.parseInt(widthString);
+          
+          String heightString = line.substring(secondX, line.size());
+          int height = Integer.parseInt(heightString);
+          
           int max = 0;
-       
-         
-            System.out.println(length + " " + width + " " + height);
             if (length >= width && length >= height){
             max = length;
             }
