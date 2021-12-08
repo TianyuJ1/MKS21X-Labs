cb9f83b652b9783acc401cbbe6a651165a31bbf1
TianyuJ1 <90489328+TianyuJ1@users.noreply.github.com>
2021-12-08 09:59:31 -0500

    Update Wrapping.java

--- 06-Advent/Wrapping.java
+++ 06-Advent/Wrapping.java
@@ -3,21 +3,28 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class Wrapping {
 public static void main(String[] args){
-/*try{*/
- File file = new File("input.txt");
+	try{
+ File file = new File(args[0]);
           Scanner input = new Scanner(file);
           int lineNum = 0;
           while (input.hasNextLine()) {
               String line = input.nextLine();
-              System.out.println(lineNum + ":  " + line);
+             /* System.out.println(lineNum + ":  " + line);*/
               lineNum++;
           }
-          System.out.println(slacks(args[0]));
-/* }
+          int mode = Integer.parseInt(args[1]);
+          if (mode == 1){
+          	System.out.println(wrapPaper(args[0]));
+          }
+          if (mode == 2){
+          	System.out.println(slacks(args[0]));
+          }
+ }
 catch(FileNotFoundException e){
         System.out.println("file not found");
-    }*/
-}
+    }
+   }
+
 public static int wrapPaper(String data){
 int WrapPaper = 0;
 
@@ -28,26 +35,117 @@ try {
           while (input.hasNextLine()){
           String line = input.nextLine();
           Scanner s = new Scanner(line);
-          String firstX = line.indexOf("x");
-          String secondX = line.lastIndexOf("x");
-          
+          int firstX = line.indexOf("x");
+          int secondX = line.lastIndexOf("x");
+
           String lengthString = line.substring(0, firstX);
           int length = Integer.parseInt(lengthString);
-          
-          String widthString = line.substring(firstX, secondX);
+
+          String widthString = line.substring(firstX +1, secondX);
           int width = Integer.parseInt(widthString);
-          
-          String heightString = line.substring(secondX, line.lastIndexOf());
+
+          String heightString = line.substring(secondX +1, line.length());
           int height = Integer.parseInt(heightString);
-          
-           WrapPaper = ((2 * length * height) + (2 * length * width) + (2 * height * width));}
+
+          int min = Math.min(length * width, Math.min(width * height, height * length));
+
+           WrapPaper += ((2 * length * height) + (2 * length * width) + (2 * height * width) + min);}
+} catch(FileNotFoundException e){
+        System.out.println("file not found");
+    }
+    return WrapPaper;
+    }import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class Wrapping {
+public static void main(String[] args){
+	try{
+ File file = new File(args[0]);
+          Scanner input = new Scanner(file);
+          int lineNum = 0;
+          while (input.hasNextLine()) {
+              String line = input.nextLine();
+             /* System.out.println(lineNum + ":  " + line);*/
+              lineNum++;
+          }
+          int mode = Integer.parseInt(args[1]);
+          if (mode == 1){
+          	System.out.println(wrapPaper(args[0]));
+          }
+          if (mode == 2){
+          	System.out.println(slacks(args[0]));
+          }
+ }
+catch(FileNotFoundException e){
+        System.out.println("file not found");
+    }
+   }
+
+public static int wrapPaper(String data){
+int WrapPaper = 0;
+
+try {
+
+          File file = new File(data);
+          Scannimport java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class Wrapping {
+public static void main(String[] args){
+	try{
+ File file = new File(args[0]);
+          Scanner input = new Scanner(file);
+          int lineNum = 0;
+          while (input.hasNextLine()) {
+              String line = input.nextLine();
+             /* System.out.println(lineNum + ":  " + line);*/
+              lineNum++;
+          }
+          int mode = Integer.parseInt(args[1]);
+          if (mode == 1){
+          	System.out.println(wrapPaper(args[0]));
+          }
+          if (mode == 2){
+          	System.out.println(slacks(args[0]));
+          }
+ }
+catch(FileNotFoundException e){
+        System.out.println("file not found");
+    }
+   }
+
+public static int wrapPaper(String data){
+int WrapPaper = 0;
+
+try {
+
+          File file = new File(data);
+          Scanner input = new Scanner(file);
+          while (input.hasNextLine()){
+          String line = input.nextLine();
+          Scanner s = new Scanner(line);
+          int firstX = line.indexOf("x");
+          int secondX = line.lastIndexOf("x");
+
+          String lengthString = line.substring(0, firstX);
+          int length = Integer.parseInt(lengthString);
+
+          String widthString = line.substring(firstX +1, secondX);
+          int width = Integer.parseInt(widthString);
+
+          String heightString = line.substring(secondX +1, line.length());
+          int height = Integer.parseInt(heightString);
+
+          int min = Math.min(length * width, Math.min(width * height, height * length));
+
+           WrapPaper += ((2 * length * height) + (2 * length * width) + (2 * height * width) + min);}
 } catch(FileNotFoundException e){
         System.out.println("file not found");
     }
     return WrapPaper;
     }
-           
-           
+
+
 public static int slacks(String data){
 int slack = 0;
 
@@ -57,18 +155,18 @@ try {
           while (input.hasNextLine()){
           String line = input.nextLine();
           Scanner s = new Scanner(line);
-          String firstX = line.indexOf("x");
-          String secondX = line.lastIndexOf("x");
-          
+          int firstX = line.indexOf("x");
+          int secondX = line.lastIndexOf("x");
+
           String lengthString = line.substring(0, firstX);
           int length = Integer.parseInt(lengthString);
-          
-          String widthString = line.substring(firstX, secondX);
+
+          String widthString = line.substring(firstX +1, secondX);
           int width = Integer.parseInt(widthString);
-          
-          String heightString = line.substring(secondX, line.length()-1);
+
+          String heightString = line.substring(secondX +1, line.length());
           int height = Integer.parseInt(heightString);
-          
+
           int max = 0;
             if (length >= width && length >= height){
             max = length;
@@ -80,7 +178,116 @@ try {
             max = width;
             }
             slack = slack + ((length + width + height - max)*2 + length * width * height);
-           
+
+            }
+            }
+            catch(FileNotFoundException e){
+        System.out.println("file not found");
+    }
+    return slack;
+    }
+    }er input = new Scanner(file);
+          while (input.hasNextLine()){
+          String line = input.nextLine();
+          Scanner s = new Scanner(line);
+          int firstX = line.indexOf("x");
+          int secondX = line.lastIndexOf("x");
+
+          String lengthString = line.substring(0, firstX);
+          int length = Integer.parseInt(lengthString);
+
+          String widthString = line.substring(firstX +1, secondX);
+          int width = Integer.parseInt(widthString);
+
+          String heightString = line.substring(secondX +1, line.length());
+          int height = Integer.parseInt(heightString);
+
+          int min = Math.min(length * width, Math.min(width * height, height * length));
+
+           WrapPaper += ((2 * length * height) + (2 * length * width) + (2 * height * width) + min);}
+} catch(FileNotFoundException e){
+        System.out.println("file not found");
+    }
+    return WrapPaper;
+    }
+
+
+public static int slacks(String data){
+int slack = 0;
+
+try {
+        File file = new File(data);
+          Scanner input = new Scanner(file);
+          while (input.hasNextLine()){
+          String line = input.nextLine();
+          Scanner s = new Scanner(line);
+          int firstX = line.indexOf("x");
+          int secondX = line.lastIndexOf("x");
+
+          String lengthString = line.substring(0, firstX);
+          int length = Integer.parseInt(lengthString);
+
+          String widthString = line.substring(firstX +1, secondX);
+          int width = Integer.parseInt(widthString);
+
+          String heightString = line.substring(secondX +1, line.length());
+          int height = Integer.parseInt(heightString);
+
+          int max = 0;
+            if (length >= width && length >= height){
+            max = length;
+            }
+            if (height >= width && height >= length){
+            max = height;
+            }
+            if (width >= length && width >= height){
+            max = width;
+            }
+            slack = slack + ((length + width + height - max)*2 + length * width * height);
+
+            }
+            }
+            catch(FileNotFoundException e){
+        System.out.println("file not found");
+    }
+    return slack;
+    }
+    }
+
+
+public static int slacks(String data){
+int slack = 0;
+
+try {
+        File file = new File(data);
+          Scanner input = new Scanner(file);
+          while (input.hasNextLine()){
+          String line = input.nextLine();
+          Scanner s = new Scanner(line);
+          int firstX = line.indexOf("x");
+          int secondX = line.lastIndexOf("x");
+
+          String lengthString = line.substring(0, firstX);
+          int length = Integer.parseInt(lengthString);
+
+          String widthString = line.substring(firstX +1, secondX);
+          int width = Integer.parseInt(widthString);
+
+          String heightString = line.substring(secondX +1, line.length());
+          int height = Integer.parseInt(heightString);
+
+          int max = 0;
+            if (length >= width && length >= height){
+            max = length;
+            }
+            if (height >= width && height >= length){
+            max = height;
+            }
+            if (width >= length && width >= height){
+            max = width;
+            }
+            slack = slack + ((length + width + height - max)*2 + length * width * height);
+
             }
             }
             catch(FileNotFoundException e){
