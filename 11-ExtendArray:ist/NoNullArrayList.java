09c8536ddf9092ae8ee732b9aca32d5269bdb2d2
Tianyu Jiao <tjiao30@stuy.edu>
2021-12-16 10:14:48 -0500

    working on addAll

---
+++ 11-ExtendArray:ist/NoNullArrayList.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+public class NoNullArrayList<T> extends ArrayList<T> {
+  public statis void Main(String[]args){
+  }
+
+  public addAll(T start){
+    try{
+      value = start;
+      for(int i=0; i < ArrayList.length; i++){
+        value = T.setValue()
+      }
+    }catch(illegalArgumentException e){
+      System.out.println("error");
+    }
+  }
+
+  public setValue(T newValueS)
+}
