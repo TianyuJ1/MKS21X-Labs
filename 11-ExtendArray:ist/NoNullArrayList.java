c1b1e81f044dd99bfdb6fd522797b8c157f27d78
Tianyu Jiao <tjiao30@stuy.edu>
2021-12-17 10:11:05 -0500

    working on boolean add

--- 11-ExtendArray:ist/NoNullArrayList.java
+++ 11-ExtendArray:ist/NoNullArrayList.java
@@ -1,18 +1,44 @@
 import java.util.ArrayList;
 public class NoNullArrayList<T> extends ArrayList<T> {
-  public statis void Main(String[]args){
+  /*public statis void Main(String[]args){
+    System.out.println(super.addAll();
+
+  }*/
+  public NoNullArrayList() {
+    super();
   }
 
-  public addAll(T start){
+  public NoNullArrayList(int startingCapacity) {
+    super(startingCapacity);
+  }
+
+  public addAll(int index, T value){
     try{
-      value = start;
-      for(int i=0; i < ArrayList.length; i++){
-        value = T.setValue()
+      value = "null"
+      return super.addAll(index, value);
       }
     }catch(illegalArgumentException e){
       System.out.println("error");
     }
   }
 
-  public setValue(T newValueS)
+  public boolean add(T value) {
+   try{
+    if (val == null) {
+    return true;}
+   }catch(illegalArgumentException ex){
+    System.out.println("error");
+    }
+   return super.add(value);
+  }
+
+  public void add(int index, T value) {
+   try{
+    if (val == null) {
+      return super.add(index, value);
+    }catch(illegalArgumentException ex){
+      System.out.println("error");
+    }
+  }
+ }
 }
