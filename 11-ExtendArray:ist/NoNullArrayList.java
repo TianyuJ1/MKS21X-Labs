d69e691040188497acb36af8261f842ecdec9b56
Tianyu Jiao <tjiao30@stuy.edu>
2021-12-21 10:14:36 -0500

    fixed add method

--- 11-ExtendArray:ist/NoNullArrayList.java
+++ 11-ExtendArray:ist/NoNullArrayList.java
@@ -16,25 +16,23 @@ public class NoNullArrayList<T> extends ArrayList<T> {
       if (value == null)
     }throw new(illegalArgumentException e){
       System.out.println("error");
-    }
+
     return super.addAll(index, value);
   }
+}
 
   public boolean add(T value) {
     if (val == null) {
-    return true;
-  }throw new(illegalArgumentException ex){
+  throw new(illegalArgumentException ex){
     System.out.println("error");
-    }
+}
+
    return super.add(value);
   }
 
   public void add(int index, T value) {
     if (val == null) {
-      return super.add(index, value);
-    }throw new(illegalArgumentException ex){
+      throw new(illegalArgumentException ex){
       System.out.println("error");
     }
-  }
  }
-}
